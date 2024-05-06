857ba8afb13935f381c8a3a04949702048fac551
donnysait88 <112736862+rizbikit1781@users.noreply.github.com>
2024-05-06 00:46:08 -0600

    Set up database connection

---
+++ lib/database/models/event.model.ts
@@ -0,0 +1,38 @@
+import { Document } from "mongodb";
+import { Schema, Types, model, models } from "mongoose";
+
+export interface IEvent extends Document {
+    _id: string;
+    title: string;
+    description?: string;
+    location?: string;
+    createAt: Date;
+    imageUrl: string;
+    startDateTime: Date;
+    endDateTime: Date;
+    price?: string;
+    url?: string;
+    isFree: boolean;
+    category: { _id: string, name: string}
+    organizer: { _id: string, firstName: string, lastName: string}
+}
+
+
+const EventSchema = new Schema({
+    title: { type: String, required: true},
+    description: { type: String},
+    location: { type: String},
+    createAt: { type: Date, default: Date.now},
+    imageUrl: { type: String, required: true},
+    startDateTime: { type: Date, default: Date.now},
+    endDateTime: { type: Date, default: Date.now},
+    price: { type: String },
+    url: { type: String},
+    isFree: { type: Boolean, default: false},
+    category: { type: Schema.Types.ObjectId, ref: 'Category'},
+    organizer: { type: Schema.Types.ObjectId, ref: 'User'},
+})
+
+const Event = models.Event || model('Event', EventSchema);
+
+export default Event;
--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,8 +1,15 @@
-import { Schema } from "mongoose"
-import { unique } from "next/dist/build/utils"
+import { Schema, model, models } from "mongoose";
 
 
-const userSchema = new Schema({
-    clerkId: { type : String, required: true, unique: true},
-    email: { type: String, required: true, unique: true}
-})
+const UserSchema = new Schema({
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  photo: { type: String, required: true },
+});
+
+const User = models.User || model('User', UserSchema);
+
+export default User;
