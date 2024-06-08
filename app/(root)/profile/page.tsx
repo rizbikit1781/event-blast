51ce4a35adf4f46eff052163164c64e6a4697aee
rizbikit1781 <112736862+rizbikit1781@users.noreply.github.com>
2024-06-07 18:18:49 -0600

    Implemented Stripe

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,10 +1,16 @@
 import Collection from '@/components/shared/Collection'
 import { Button } from '@/components/ui/button'
+import { getEventsByUser } from '@/lib/actions/event.actions'
+import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
 
-const ProfilePage = () => {
+const ProfilePage = async () => {
 
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
+
+  const organizedEvents = await getEventsByUser({ userId, page: 1 })
 
   return (
     <>
@@ -12,7 +18,7 @@ const ProfilePage = () => {
         <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
             <div className='wrapper flex items-center justify-center sm:justify-between'>
                 <h3 className='h3-bold text-center sm:text-left'>My Tickets</h3>
-                <Button asChild className='button hidden sm:flex'>
+                <Button asChild size="lg" className='button hidden sm:flex'>
                     <Link href="/#event" >Explore More Events</Link>
                 </Button>
             
@@ -23,7 +29,7 @@ const ProfilePage = () => {
         {/* <section className='wrapper my-8'>
             <Collection 
             data={events?.data}
-            emptyTitle="No Events ticket purchased yey"
+            emptyTitle="No Events ticket purchased yet"
             emptyStateSubtext="Stay Tune for our super exciting events coming!!"
             collectionType="My_Tickets"
             limit={3}
@@ -33,16 +39,29 @@ const ProfilePage = () => {
             />
         </section> */}
 
-        {/*My Organizer*/}
+        {/* Events Organized */}
         <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
             <div className='wrapper flex items-center justify-center sm:justify-between'>
                 <h3 className='h3-bold text-center sm:text-left'>Events Organized</h3>
-                <Button asChild className='button hidden sm:flex'>
-                    <Link href="/#events/create" >Create New Event</Link>
+                <Button asChild size="lg" className='button hidden sm:flex'>
+                    <Link href="/events/create" >Create New Event</Link>
                 </Button>
-            
+
             </div>  
 
+        </section>
+
+        <section className='wrapper my-8'>
+            <Collection 
+            data={organizedEvents?.data}
+            emptyTitle="No Events have been created yet"
+            emptyStateSubtext="Let create your own event!!"
+            collectionType="Events_Organized"
+            limit={6}
+            page={1}
+            urlParamName='eventsPage'
+            totalPages={2}
+            />
         </section>
     </>
   )
--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -41,17 +41,19 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
         )}
 
 
-        <Link 
-        href={`/events/${event._id}`}
-        className='flex min-h-[230px] flex-col gap-3 p-5 md:gap-4'
-        >
+        <div className='flex min-h-[230px] flex-col gap-3 p-5 md:gap-4'> 
+      
           {!hidePrice && <div className='flex gap-2'>
             <span className='p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60'>{event.isFree ? 'FREE' : `$${event.price}`}</span>
             <p className='p-semibold-14 w-min rounded-full px-4 py-1 bg-grey-500/10 text-grey-500 line-clamp-1'>{event.category.name}</p>
           </div>}
 
           <p className='p-medium-16 md:p-medium-18 text-gray-500'>{formatDateTime(event.startDateTime).dateTime}</p>
-          <p className='p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black'>{event.title}</p>
+          <Link
+            href={`/events/${event._id}`}
+          >
+            <p className='p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black'>{event.title}</p>
+          </Link>
           <div className='flex-between w-full'>
             <p className='p-medium-14 md:p-medium-16 text-grey-600'> 
               {event.organizer.firstName} {event.organizer.lastName}
@@ -70,7 +72,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
               </Link>
             )}
           </div>
-        </Link>
+        </div>
     </div>
   )
 }
