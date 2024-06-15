b43a3b276e811477615916528a1d7b6defefb412
rizbikit1781 <112736862+rizbikit1781@users.noreply.github.com>
2024-06-14 23:56:43 -0600

    Update Search.tsx

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import Image from 'next/image'
+import React, { useState } from 'react'
 
 const Search = () => {
+
+  const [query, setQuery] = useState("")
+
   return (
-    <div>Search</div>
+    <div className='flex-center min-h-[54px] w-full overflow-hidden rounded-full bg-grey=50 px-4 py-2'>
+      <Image 
+        src="/assets/icons/search.svg"
+        alt="search"
+        width={24}
+        height={24}
+      />
+    </div>
   )
 }
 
