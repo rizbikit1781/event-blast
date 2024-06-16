c7ee96598964c35894d307597aefdffaadaf8389
rizbikit1781 <112736862+rizbikit1781@users.noreply.github.com>
2024-06-16 14:14:45 -0600

    Update search and category fileter

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -1,50 +1,55 @@
-"use client"
+"use client";
 
-import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
-import { Input } from '../ui/input'
-import { formUrlQuery, removeKeysFromQuery } from '@/lib/utils'
-import { useSearchParams } from 'next/navigation'
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { Input } from "../ui/input";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
 
-const Search = () => {
-
-  const [query, setQuery] = useState("")
+const Search = ({ placeholder = "Search Title..."}: {placeholder? : string}) => {
+  const [query, setQuery] = useState("");
+  const router = useRouter();
   const searchParams = useSearchParams();
 
   useEffect(() => {
     const delayDebounceFn = setTimeout(() => {
-      if(query) {
-        const newUrl = formUrlQuery({
+      let newUrl = ""
+
+      if (query) {
+        newUrl = formUrlQuery({
           params: searchParams.toString(),
-          key: 'query',
-          value: query
-        })
+          key: "query",
+          value: query,
+        });
       } else {
-        const newUrl = removeKeysFromQuery({
+        newUrl = removeKeysFromQuery({
           params: searchParams.toString(),
-          keysToRemove: ['query']
-      })
-    }
-    
-    }, 300)
-  }, [query])
+          keysToRemove: ["query"],
+        });
+      }
+      router.push(newUrl, { scroll: false })
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+
+  }, [query, searchParams, router]);
 
   return (
-    <div className='flex-center min-h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2'>
-      <Image 
+    <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+      <Image
         src="/assets/icons/search.svg"
         alt="search"
         width={24}
         height={24}
       />
-      <Input 
-        type='text'
-        placeholder='Search'
+      <Input
+        type="text"
+        placeholder={placeholder}
         onChange={(e) => setQuery(e.target.value)}
-        className='p-regular-16 border-0 bg-grey-50 outline-offset-0 placeholder:text-grey-500 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0'
+        className="p-regular-16 border-0 bg-grey-50 outline-offset-0 placeholder:text-grey-500 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
       />
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
